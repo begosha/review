81b51174cd97f7c1caccf2507425928bd52ec5ae
Gosha <begoshaaaa@gmail.com>
2021-05-01 16:24:08 +0600

    new model profile is added

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='User')
+    github = models.URLField(max_length=1000, null=True, blank=True, verbose_name='Github')
+    summary = models.TextField(null=True, blank=True, verbose_name='About')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Avatar')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
